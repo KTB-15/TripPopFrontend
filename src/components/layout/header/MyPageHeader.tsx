4b4ee12a09965a5cef3bdb2d5c1f95d07814b72a
Sin Han <vrdhan212@gmail.com>
2024-08-15 16:53:38 +0900

    add: mypage header

---
+++ src/components/layout/header/MyPageHeader.tsx
@@ -0,0 +1,14 @@
+import MyPageNavSection from '@/components/layout/header/nav/UserInfoNavSection';
+
+const MyPageHeader = () => {
+  return (
+    <>
+      <header className="px-3 py-2 flex items-center border justify-between">
+        <h1 className="text-3xl font-bold">TripPop!</h1>
+        <MyPageNavSection></MyPageNavSection>
+      </header>
+    </>
+  );
+};
+
+export default MyPageHeader;
