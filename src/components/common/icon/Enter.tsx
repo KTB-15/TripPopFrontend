66b05fe04e4da628012204007c487c2aeddd11c0
Sin Han <vrdhan212@gmail.com>
2024-08-16 17:31:58 +0900

    update: set hover color for enter icon

--- src/components/common/icon/Enter.tsx
+++ src/components/common/icon/Enter.tsx
@@ -6,7 +6,7 @@ const EnterSVG = () => {
       viewBox="0 0 24 24"
       strokeWidth={1.5}
       stroke="currentColor"
-      className="size-6">
+      className="size-6 hover:stroke-blue-light">
       <path strokeLinecap="round" strokeLinejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
     </svg>
   );
