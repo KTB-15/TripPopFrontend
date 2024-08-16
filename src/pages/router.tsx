910263ebcda0e4aa6deb3e0618b828aaa8c72c01
Sin Han <vrdhan212@gmail.com>
2024-08-16 21:20:39 +0900

    refactor: set sub routes

--- src/pages/router.tsx
+++ src/pages/router.tsx
@@ -1,25 +1,36 @@
-import { createBrowserRouter } from 'react-router-dom';
-import MainPage from '@/pages/MainPage';
+import { createBrowserRouter, RouteObject } from 'react-router-dom';
 import UserFavouritePage from '@/pages/userInfo/UserFavouritePage';
 import UserReviewPage from '@/pages/userInfo/UserReviewPage';
 import UserHistoryPage from '@/pages/userInfo/UserHistoryPage';
+import RootPage from '@/pages/RootPage';
+import MainPage from '@/pages/MainPage';
+
+const myPageRoutes: RouteObject[] = [
+  {
+    path: 'mypage/favourite',
+    element: <UserFavouritePage></UserFavouritePage>,
+  },
+  {
+    path: 'mypage/review',
+    element: <UserReviewPage></UserReviewPage>,
+  },
+  {
+    path: 'mypage/history',
+    element: <UserHistoryPage></UserHistoryPage>,
+  },
+];
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <MainPage></MainPage>,
-  },
-  {
-    path: '/mypage/favourite',
-    element: <UserFavouritePage></UserFavouritePage>,
-  },
-  {
-    path: '/mypage/review',
-    element: <UserReviewPage></UserReviewPage>,
-  },
-  {
-    path: '/mypage/history',
-    element: <UserHistoryPage></UserHistoryPage>,
+    element: <RootPage></RootPage>,
+    children: [
+      {
+        index: true,
+        element: <MainPage />,
+      },
+      ...myPageRoutes,
+    ],
   },
 ]);
 
