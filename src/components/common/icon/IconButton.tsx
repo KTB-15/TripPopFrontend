d944901659df62fe8de4016714c5e871b0675c9b
Sin Han <vrdhan212@gmail.com>
2024-08-17 20:39:45 +0900

    update: add circle border prop for icon

--- src/components/common/icon/IconButton.tsx
+++ src/components/common/icon/IconButton.tsx
@@ -1,12 +1,15 @@
 interface IconButtonProps {
   children: React.ReactNode;
   onClick?: () => void;
+  rounded?: boolean;
 }
 
-const IconButton = ({ children, onClick }: IconButtonProps) => {
+const IconButton = ({ children, onClick, rounded }: IconButtonProps) => {
   return (
     <>
-      <div onClick={onClick} className="cursor-pointer">
+      <div
+        onClick={onClick}
+        className={`cursor-pointer ${rounded ? 'border-option rounded-full border px-1 py-1' : ''}`}>
         {children}
       </div>
     </>
