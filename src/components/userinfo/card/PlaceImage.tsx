6ab3328cb8180daad393ed7c94559dc90be91711
Sin Han <vrdhan212@gmail.com>
2024-08-17 19:55:24 +0900

    refactor: set image min width in card

--- src/components/userinfo/card/PlaceImage.tsx
+++ src/components/userinfo/card/PlaceImage.tsx
@@ -5,7 +5,7 @@ interface PlaceImageProps {
 
 const PlaceImage = ({ src, alt }: PlaceImageProps) => {
   return (
-    <div className="overflow-hidden rounded-lg">
+    <div className="overflow-hidden rounded-lg min-w-60">
       <img className="object-fill hover:animate-zoom-in h-60 w-60" src={src} alt={alt} />
     </div>
   );
