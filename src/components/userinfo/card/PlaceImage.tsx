c893ff91778bb0711c9552088804cd31069f9456
Sin Han <vrdhan212@gmail.com>
2024-08-17 20:08:24 +0900

    refactor: responsive card image size

--- src/components/userinfo/card/PlaceImage.tsx
+++ src/components/userinfo/card/PlaceImage.tsx
@@ -5,8 +5,8 @@ interface PlaceImageProps {
 
 const PlaceImage = ({ src, alt }: PlaceImageProps) => {
   return (
-    <div className="overflow-hidden rounded-lg min-w-60">
-      <img className="object-fill hover:animate-zoom-in h-60 w-60" src={src} alt={alt} />
+    <div className="overflow-hidden rounded-lg min-w-40 sm:min-w-60">
+      <img className="object-fill w-40 h-40 hover:animate-zoom-in sm:h-60 sm:w-60" src={src} alt={alt} />
     </div>
   );
 };
