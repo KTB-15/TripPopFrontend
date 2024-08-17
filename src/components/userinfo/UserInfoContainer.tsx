a9e6b18ba2f7cb985612de20196603427abe4182
Sin Han <vrdhan212@gmail.com>
2024-08-17 20:41:49 +0900

    remove: border of favourite card container

--- src/components/userinfo/UserInfoContainer.tsx
+++ src/components/userinfo/UserInfoContainer.tsx
@@ -1,7 +1,7 @@
 import { ReactNode } from 'react';
 
 const UserInfoContainer = ({ children }: { children: ReactNode }) => {
-  return <div className="flex h-full flex-col space-y-8 border px-[6rem] py-8 md:py-4">{children}</div>;
+  return <div className="flex h-full flex-col space-y-8 px-[6rem] py-8 md:py-4">{children}</div>;
 };
 
 export default UserInfoContainer;
