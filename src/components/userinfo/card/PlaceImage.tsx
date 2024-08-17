b33fd4c20ba7d85c6c8c558fae28cf0317165d55
Sin Han <vrdhan212@gmail.com>
2024-08-17 15:35:06 +0900

    add: place image in user info

---
+++ src/components/userinfo/card/PlaceImage.tsx
@@ -0,0 +1,14 @@
+interface PlaceImageProps {
+  src: string;
+  alt: string;
+}
+
+const PlaceImage = ({ src, alt }: PlaceImageProps) => {
+  return (
+    <div className="overflow-hidden rounded-lg">
+      <img className="object-fill hover:animate-zoom-in h-60 w-60" src={src} alt={alt} />
+    </div>
+  );
+};
+
+export default PlaceImage;
