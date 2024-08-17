eb56c56f6e9bc9e4ba5d1bd573f7aef0d272376e
Sin Han <vrdhan212@gmail.com>
2024-08-17 20:40:08 +0900

    update: add option to user info card

--- src/components/userinfo/card/PlaceInfo.tsx
+++ src/components/userinfo/card/PlaceInfo.tsx
@@ -2,7 +2,7 @@ import { Hashtag } from '@/components/common';
 
 const PlaceInfo = () => {
   return (
-    <div className="flex min-w-24 flex-col items-start justify-start space-y-4 sm:min-w-[12rem]">
+    <div className="flex w-24 min-w-24 flex-col items-start justify-start space-y-4 sm:w-[12rem] sm:min-w-[12rem]">
       <h1 className="text-lg font-semibold sm:text-3xl">여행지 이름</h1>
       <div className="flex flex-wrap gap-2">
         <Hashtag text="전주" />
--- src/components/userinfo/card/UserInfoCard.tsx
+++ src/components/userinfo/card/UserInfoCard.tsx
@@ -1,8 +1,11 @@
 import { ReactNode } from 'react';
 
-const UserInfoCard = ({ children }: { children: ReactNode }) => {
+const UserInfoCard = ({ children, options }: { children: ReactNode; options?: ReactNode }) => {
   return (
-    <div className="flex space-x-6 rounded-lg border px-6 py-2 shadow-[4px_4px_4px_rgba(0,0,0,0.3)]">{children}</div>
+    <div className="flex items-center space-x-6 rounded-lg px-6 py-2 shadow-[4px_4px_4px_rgba(0,0,0,0.3)]">
+      {children}
+      <div className="absolute right-[7rem] top-6 flex space-x-2">{options}</div>
+    </div>
   );
 };
 
