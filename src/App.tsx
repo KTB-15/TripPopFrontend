ac153eb4474f55e21c9bff4857a284e100fe85a0
teddy.kim <qazkyj0310@gmail.com>
2024-08-20 15:13:07 +0900

    add: react query

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { RouterProvider } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import router from '@/pages/router';
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <>
-      <RouterProvider router={router}></RouterProvider>
-    </>
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   );
 }
 
