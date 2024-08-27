84298b691d476c487eccbd0b2d0d922bad81528e
Sin Han <vrdhan212@gmail.com>
2024-08-27 10:34:30 +0900

    update: survey card progress using store

--- src/components/survey/SurveyCard.tsx
+++ src/components/survey/SurveyCard.tsx
@@ -1,12 +1,14 @@
 import SurveyCardBody from '@/components/survey/SurveyCardBody';
 import SurveyCardFooter from '@/components/survey/SurveyCardFooter';
 import SurveyCardHeader from '@/components/survey/SurveyCardHeader';
+import useSurveyStore from '@/stores/SurveyStore';
 
-// TODO: 진행률 prop
 const SurveyCard = () => {
+  const surveyStore = useSurveyStore();
+
   return (
     <section className="flex min-w-[350px] flex-col space-y-5 rounded-lg border bg-white px-10 py-5 sm:w-[70vh]">
-      <SurveyCardHeader progress={1} />
+      <SurveyCardHeader progress={surveyStore.progress} />
       <SurveyCardBody question="Q1. 자연 vs 도시" />
       <SurveyCardFooter />
     </section>
--- src/components/survey/SurveyCardFooter.tsx
+++ src/components/survey/SurveyCardFooter.tsx
@@ -1,12 +1,36 @@
 import SurveyButton from '@/components/survey/SurveyButton';
+import useSurveyStore from '@/stores/SurveyStore';
 
 const SurveyCardFooter = () => {
+  const surveyStore = useSurveyStore();
+  const onNextClick = () => {
+    surveyStore.incProgress();
+  };
+
+  const onPrevClick = () => {
+    surveyStore.decProgress();
+  };
+  const onSkipClick = () => {
+    surveyStore.resetProgress();
+  };
   return (
     <div className="flex w-full items-center justify-between space-y-1">
-      <SurveyButton buttonName="뒤로가기" className="survey-btn bg-blue-survey-btn hover:bg-blue-dark" />
+      <SurveyButton
+        buttonName="뒤로가기"
+        onClick={onPrevClick}
+        className="survey-btn bg-blue-survey-btn hover:bg-blue-dark"
+      />
       <div className="flex items-center justify-center space-x-5">
-        <SurveyButton buttonName="건너뛰기" className="survey-btn bg-cancel text-black hover:bg-gray-400" />
-        <SurveyButton buttonName="다음" className="survey-btn bg-blue-survey-btn hover:bg-blue-dark" />
+        <SurveyButton
+          buttonName="건너뛰기"
+          onClick={onSkipClick}
+          className="survey-btn bg-cancel text-black hover:bg-gray-400"
+        />
+        <SurveyButton
+          buttonName="다음"
+          onClick={onNextClick}
+          className="survey-btn bg-blue-survey-btn hover:bg-blue-dark"
+        />
       </div>
     </div>
   );
