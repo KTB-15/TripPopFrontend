4f446f0833e9f9a14ccea5a476e3405e77115ff3
Sin Han <vrdhan212@gmail.com>
2024-08-27 10:24:40 +0900

    update: survey circle style

--- src/components/survey/SurveyCard.tsx
+++ src/components/survey/SurveyCard.tsx
@@ -5,7 +5,7 @@ import SurveyCardHeader from '@/components/survey/SurveyCardHeader';
 // TODO: 진행률 prop
 const SurveyCard = () => {
   return (
-    <section className="flex w-[70vh] min-w-[350px] flex-col space-y-5 rounded-lg border bg-white px-10 py-5">
+    <section className="flex min-w-[350px] flex-col space-y-5 rounded-lg border bg-white px-10 py-5 sm:w-[70vh]">
       <SurveyCardHeader progress={1} />
       <SurveyCardBody question="Q1. 자연 vs 도시" />
       <SurveyCardFooter />
