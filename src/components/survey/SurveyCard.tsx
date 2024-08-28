7a866fa4637c2f1a08ae5ac9f2c402b71e867f5e
Sin Han <vrdhan212@gmail.com>
2024-08-28 14:28:39 +0900

    update: question binding to survey card

--- src/components/survey/SurveyCard.tsx
+++ src/components/survey/SurveyCard.tsx
@@ -1,15 +1,15 @@
 import SurveyCardBody from '@/components/survey/SurveyCardBody';
 import SurveyCardFooter from '@/components/survey/SurveyCardFooter';
 import SurveyCardHeader from '@/components/survey/SurveyCardHeader';
-import useSurveyStore from '@/stores/SurveyStore';
+import useSurveyStore, { QUESTIONS } from '@/stores/SurveyStore';
 
 const SurveyCard = () => {
   const surveyStore = useSurveyStore();
 
   return (
-    <section className="flex min-w-[350px] flex-col space-y-5 rounded-lg border bg-white px-10 py-5 sm:w-[70vh]">
+    <section className="flex w-[375px] min-w-[375px] flex-col space-y-5 rounded-lg border bg-white px-10 py-5 sm:w-[80vh]">
       <SurveyCardHeader progress={surveyStore.progress} />
-      <SurveyCardBody question="Q1. 자연 vs 도시" />
+      <SurveyCardBody question={QUESTIONS[surveyStore.progress]} />
       <SurveyCardFooter />
     </section>
   );
--- src/components/survey/SurveyCardBody.tsx
+++ src/components/survey/SurveyCardBody.tsx
@@ -5,7 +5,7 @@ interface SurveyCardBodyProps {
 const SurveyCardBody = ({ question }: SurveyCardBodyProps) => {
   return (
     <div className="flex w-full flex-col items-center justify-center space-y-2 overflow-hidden">
-      <h1 className="max-w-full break-words text-center text-xl font-bold sm:text-3xl">{question}</h1>
+      <h1 className="max-w-full break-words text-center text-xl font-bold sm:text-2xl">{question}</h1>
       <section className="flex w-full flex-1 items-center justify-evenly">
         <div className="border-survey-left hover:bg-survey-left survey-circle xl-circle"></div>
         <div className="border-survey-left hover:bg-survey-left survey-circle lg-circle"></div>
