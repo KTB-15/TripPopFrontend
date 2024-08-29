e3612d3e4b68cf4f639efc0906dd232bfd231073
Sin Han <vrdhan212@gmail.com>
2024-08-29 17:21:34 +0900

    add: request type for survey

--- src/data/type.ts
+++ src/data/type.ts
@@ -1,5 +1,6 @@
 // API 요청/ 응답 형식
 
+// 추천 이미지
 export interface RecommendedPlaceReq {
   places: number[];
 }
@@ -11,4 +12,16 @@ export interface PlaceRes {
 
 export interface RecommendedPlaceRes {
   places: PlaceRes[];
+}
+
+// 설문지
+export interface SurveyReq {
+  travelStyle1: number;
+  travelStyle2: number;
+  travelStyle3: number;
+  travelStyle4: number;
+  travelStyle5: number;
+  travelStyle6: number;
+  travelStyle7: number;
+  travelStyle8: number;
 }
