88d0049ac7fb682a435c1e2f2db2cbddcfe01958
Sin Han <vrdhan212@gmail.com>
2024-09-01 22:20:20 +0900

    refactor: user info card

--- src/components/userinfo/card/UserInfoCard.tsx
+++ src/components/userinfo/card/UserInfoCard.tsx
@@ -1,9 +1,16 @@
+import { InfoSection, PlaceImage, PlaceInfo } from '@/components/userinfo/card';
 import { ReactNode } from 'react';
 
-const UserInfoCard = ({ children, options }: { children: ReactNode; options?: ReactNode }) => {
+const UserInfoCard = ({ options }: { options?: ReactNode }) => {
   return (
     <div className="flex items-center space-x-6 rounded-lg px-6 py-2 shadow-[4px_4px_4px_rgba(0,0,0,0.3)]">
-      {children}
+      <PlaceImage
+        src="https://www.travelonline.com/thailand/attractions/white-temple/thailand-generic-white-temple-hd-56217-banner.jpg"
+        alt="White Temple"
+      />
+      <InfoSection>
+        <PlaceInfo />
+      </InfoSection>
       <div className="absolute right-[7rem] top-6 flex space-x-2">{options}</div>
     </div>
   );
