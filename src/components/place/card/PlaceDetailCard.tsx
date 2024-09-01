9c32bb51fc130314bd9b94ffb96a3574616a8535
Sin Han <vrdhan212@gmail.com>
2024-09-01 14:37:49 +0900

    update: style for place detail card

--- src/components/place/card/PlaceDetailCard.tsx
+++ src/components/place/card/PlaceDetailCard.tsx
@@ -2,19 +2,19 @@ import { Hashtag } from '@/components/common';
 import { Favourite, Bookmark, Star } from '@/components/common/icon';
 
 const CardBody = () => (
-  <div className="p-4">
+  <div className="space-y-2 p-4">
     <div className="flex items-center justify-between">
-      <h2 className="text-2xl font-bold">여행지 이름</h2>
+      <h2 className="text-3xl font-bold">여행지 이름</h2>
       <div className="flex items-center space-x-2">
         <Favourite />
         <Bookmark />
+        <div className="flex items-center">
+          <Star />
+          <span className="ml-1">4.6 / 5</span>
+        </div>
       </div>
     </div>
-    <div className="mt-2 flex items-center">
-      <Star />
-      <span className="ml-1">4.6 / 5</span>
-    </div>
-    <div className="mt-2 flex space-x-2">
+    <div className="flex space-x-2">
       <Hashtag text="경주" />
     </div>
   </div>
@@ -22,9 +22,13 @@ const CardBody = () => (
 
 const PlaceDetailCard = () => {
   return (
-    <div className="w-full rounded-lg bg-white shadow">
-      <div className="relative h-64 sm:h-80">
-        <img src="/api/placeholder/800/600" alt="Travel Destination" className="h-full w-full object-cover" />
+    <div className="w-full rounded-lg bg-white px-2 py-2 shadow-md sm:space-y-4">
+      <div className="h-64 overflow-hidden rounded-lg sm:h-[40rem]">
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzk92qOx7c5k5fybjVbUkwg6BGW_ptjgID9A&s"
+          alt="여행 카드 이미지"
+          className="h-full w-full object-cover"
+        />
       </div>
       <CardBody />
     </div>
