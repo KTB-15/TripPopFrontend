af6762417c4cc7ad1c06140b95c5719ddc12bad8
Sin Han <vrdhan212@gmail.com>
2024-09-01 13:29:49 +0900

    update: place card section in detail page

--- src/components/place/PlaceDetailSection.tsx
+++ src/components/place/PlaceDetailSection.tsx
@@ -1,5 +1,11 @@
+import { PlaceDetailCard } from '@/components/place/card';
+
 const PlaceDetailSection = () => {
-  return <section className="mb-4 h-full bg-blue-dark sm:mb-0 sm:mr-4 sm:w-1/2 lg:w-2/5"></section>;
+  return (
+    <section className="mb-4 flex h-full items-center justify-center bg-blue-dark sm:mb-0 sm:mr-4 sm:w-1/2 lg:w-2/5">
+      <PlaceDetailCard />
+    </section>
+  );
 };
 
 export default PlaceDetailSection;
--- src/pages/PlaceDetailPage.tsx
+++ src/pages/PlaceDetailPage.tsx
@@ -1,8 +1,12 @@
+import PlaceDetailSection from '@/components/place/PlaceDetailSection';
+import PlaceReviewSection from '@/components/place/PlaceReviewSection';
+
 const PlaceDetailPage = () => {
   return (
     <>
-      <div>
-        <h1>PlaceDetailPage</h1>
+      <div className="flex h-full flex-col justify-start px-[4rem] py-8 sm:flex-row md:py-4">
+        <PlaceDetailSection />
+        <PlaceReviewSection />
       </div>
     </>
   );
