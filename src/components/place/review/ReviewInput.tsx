207227262711e1fd918ea804039a49c4ab5092eb
Sin Han <vrdhan212@gmail.com>
2024-09-01 15:51:32 +0900

    update: review input style

--- src/components/place/review/ReviewInput.tsx
+++ src/components/place/review/ReviewInput.tsx
@@ -4,7 +4,7 @@ const ReviewInput = () => {
   const [review, setReview] = useState('');
   return (
     <textarea
-      className="w-full rounded border p-2 text-lg"
+      className="w-full resize-none rounded-lg border border-option p-2 text-lg focus:outline-none"
       placeholder="소중한 리뷰를 남겨주세요."
       value={review}
       onChange={(e) => setReview(e.target.value)}
