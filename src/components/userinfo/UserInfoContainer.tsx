3497947dfee5b408e784801b8d6052d28f5cebce
Sin Han <vrdhan212@gmail.com>
2024-09-01 22:20:03 +0900

    refactor: user info page layout

--- src/components/userinfo/UserInfoContainer.tsx
+++ src/components/userinfo/UserInfoContainer.tsx
@@ -1,7 +1,8 @@
 import { ReactNode } from 'react';
 
+// 마이페이지 레이아웃
 const UserInfoContainer = ({ children }: { children: ReactNode }) => {
-  return <div className="flex h-full flex-col space-y-8 px-[6rem] py-8 md:py-4">{children}</div>;
+  return <div className="flex h-full flex-col space-y-8 px-[4rem] py-8 md:py-4 lg:px-[12rem]">{children}</div>;
 };
 
 export default UserInfoContainer;
