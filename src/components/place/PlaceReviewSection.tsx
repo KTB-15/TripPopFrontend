ce0ba52248de8977c196d978db73ecd56f13da2a
Sin Han <vrdhan212@gmail.com>
2024-09-01 13:35:40 +0900

    update: place review section in detail page

--- src/components/place/PlaceReviewSection.tsx
+++ src/components/place/PlaceReviewSection.tsx
@@ -1,5 +1,18 @@
+import ReviewCard from '@/components/place/review/ReviewCard';
+import ReviewInput from '@/components/place/review/ReviewInput';
+
 const PlaceReviewSection = () => {
-  return <section className="h-full bg-green-300 sm:flex-1"></section>;
+  return (
+    <section className="h-full bg-green-300 sm:flex-1">
+      <h1 className="mb-4 text-xl font-bold">리뷰</h1>
+      <ReviewInput />
+      <section className="space-y-4">
+        <ReviewCard rating={5} />
+        <ReviewCard rating={5} />
+        <ReviewCard rating={5} />
+      </section>
+    </section>
+  );
 };
 
 export default PlaceReviewSection;
