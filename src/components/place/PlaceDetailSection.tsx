268286f02dc97a5345a619333f1bd793112ffd75
Sin Han <vrdhan212@gmail.com>
2024-09-01 13:38:33 +0900

    remove: unnecessary background colors

--- src/components/place/PlaceDetailSection.tsx
+++ src/components/place/PlaceDetailSection.tsx
@@ -2,7 +2,7 @@ import { PlaceDetailCard } from '@/components/place/card';
 
 const PlaceDetailSection = () => {
   return (
-    <section className="mb-4 flex h-full items-center justify-center bg-blue-dark sm:mb-0 sm:mr-4 sm:w-1/2 lg:w-2/5">
+    <section className="mb-4 flex h-full items-center justify-center sm:mb-0 sm:mr-4 sm:w-1/2 lg:w-2/5">
       <PlaceDetailCard />
     </section>
   );
--- src/components/place/PlaceReviewSection.tsx
+++ src/components/place/PlaceReviewSection.tsx
@@ -3,7 +3,7 @@ import ReviewInput from '@/components/place/review/ReviewInput';
 
 const PlaceReviewSection = () => {
   return (
-    <section className="h-full bg-green-300 sm:flex-1">
+    <section className="h-full sm:flex-1">
       <h1 className="mb-4 text-xl font-bold">리뷰</h1>
       <ReviewInput />
       <section className="space-y-4">
