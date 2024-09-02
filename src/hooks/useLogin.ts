c9b53c200108274083080e48bf4cb8d5fe1509b4
teddy.kim <qazkyj0310@gmail.com>
2024-09-02 13:13:37 +0900

    refactor: `react-query` to `@tankstack/react-query`

--- src/hooks/useLogin.ts
+++ src/hooks/useLogin.ts
@@ -1,24 +1,19 @@
-import { useMutation } from 'react-query';
+import { useMutation, UseMutationResult } from '@tanstack/react-query';
 import { loginUser, LoginRequest, LoginResponse } from '@/api/auth';
 import { useAuthStore } from '@/stores/authStore';
 
-export const useLogin = () => {
+export const useLogin = (): UseMutationResult<LoginResponse, Error, LoginRequest> => {
   const setTokens = useAuthStore((state) => state.setTokens);
 
-  return useMutation<LoginResponse, Error, LoginRequest>(
-    async (loginRequest: LoginRequest) => {
-      const data = await loginUser(loginRequest);
-      return data;
+  return useMutation<LoginResponse, Error, LoginRequest>({
+    mutationFn: (loginRequest: LoginRequest) => loginUser(loginRequest),
+    onError: (error: Error) => {
+      console.error('로그인 실패:', error.message);
+      alert('아이디, 비밀번호를 확인해주세요.');
     },
-    {
-      onError: (error: Error) => {
-        console.error('로그인 실패:', error.message);
-        alert('아이디, 비밀번호를 확인해주세요.');
-      },
-      onSuccess: (data: LoginResponse) => {
-        console.log('로그인 성공:', data);
-        setTokens(data.accessToken, data.refreshToken);
-      },
-    }
-  );
+    onSuccess: (data: LoginResponse) => {
+      console.log('로그인 성공:', data);
+      setTokens(data.accessToken, data.refreshToken);
+    },
+  });
 };
