83fd1e38e294a01486d07b43f50d5d2f4198d978
teddy.kim <qazkyj0310@gmail.com>
2024-09-02 13:14:16 +0900

    remove: annotation

--- src/hooks/useLogin.ts
+++ src/hooks/useLogin.ts
@@ -9,7 +9,6 @@ export const useLogin = (): UseMutationResult<LoginResponse, Error, LoginRequest
     mutationFn: (loginRequest: LoginRequest) => loginUser(loginRequest),
     onError: (error: Error) => {
       console.error('로그인 실패:', error.message);
-      alert('아이디, 비밀번호를 확인해주세요.');
     },
     onSuccess: (data: LoginResponse) => {
       console.log('로그인 성공:', data);
