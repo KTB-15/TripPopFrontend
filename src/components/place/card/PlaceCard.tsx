b0dc4c3e2c3bd562c68f4783c0ee20b04be4c757
Sin Han <vrdhan212@gmail.com>
2024-09-03 16:43:54 +0900

    fix: omitted request body

--- src/components/place/card/PlaceCard.tsx
+++ src/components/place/card/PlaceCard.tsx
@@ -14,6 +14,7 @@ const CardActionSection = ({ place }: { place: RecommendedPlaceRes }) => {
     {
       url: '/favourite/toggle',
       method: 'POST',
+      body: { placeId: place.placeId, isFavourite: place.isFavourite },
     },
     {
       onSuccess: () => {
